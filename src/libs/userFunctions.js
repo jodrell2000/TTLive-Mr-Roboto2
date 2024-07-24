3052d26bbacd4213230a83e703ea79f12d561feb
Adam Reynolds <github@smufflersworld.com>
2024-07-24 19:28:57 +0100

    Rebuild BBBoot

--- src/libs/userFunctions.js
+++ src/libs/userFunctions.js
@@ -184,7 +184,7 @@ const userFunctions = () => {
       try {
         return await axios.post(url, payload, { headers })
       } catch ( error ) {
-        console.error( `Error calling post api...error:${error}\nurl:${url}\npayload:${payload}` );
+        console.error( `Error calling post api...error:\n${JSON.stringify(error)}\nurl:${url}\npayload:${payload}` );
         throw error;
       }
     },

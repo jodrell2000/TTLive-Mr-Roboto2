9230d39a579995f9e66173aad90824a3dc4bc17a
Adam Reynolds <github@smufflersworld.com>
2024-10-05 14:00:57 +0100

    trying to render the json

--- src/libs/startup.js
+++ src/libs/startup.js
@@ -20,7 +20,7 @@ export default async ( roomUUID, state, roomFunctions, userFunctions, chatFuncti
     await userFunctions.rebuildUserList( state, databaseFunctions );
 
     // console.log(JSON.stringify(`full state ${JSON.stringify(state, null, 2)}`))
-    console.log(JSON.stringify( `djs from state: ${JSON.stringify(state.djs, null, 2)}` ))
+    console.log(`djs from state: ${ JSON.stringify(state.djs, null, 2) }`)
     if ( userFunctions.hasDjsElement( state ) ) {
       await userFunctions.resetDJs( state.djs )
       console.log( `djList:${ userFunctions.djList() }` )

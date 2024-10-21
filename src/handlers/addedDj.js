1d4b1cb2988f6e0aae4f9e76ef51d7461fcbf59f
Adam Reynolds <github@smufflersworld.com>
2024-10-21 17:45:49 +0100

    Testing user stage ban

--- src/handlers/addedDj.js
+++ src/handlers/addedDj.js
@@ -6,15 +6,15 @@ export default async ( currentState, payload, socket, userFunctions, roomFunctio
     if (patch.path.startsWith('/djs/')) {
       const theUserID = patch.value.uuid;
       
-      [ OKToDJ, theMessage ] = await userFunctions.checkOKToDJ( theUserID, roomFunctions );
-
-      if ( !OKToDJ ) {
-        const theUsername = await userFunctions.getUsername( theUserID )
-        const theMessage = `@${theUsername} is not allowed to DJ so was removed. Please speak to a Mod to find out why`;
-        await userFunctions.removeDJ( theUserID, theMessage, socket );
-        // await userFunctions.incrementSpamCounter( theUserID, databaseFunctions );
-        await chatFunctions.botSpeak( theMessage );
-      } else {
+      // [ OKToDJ, theMessage ] = await userFunctions.checkOKToDJ( theUserID, roomFunctions );
+      //
+      // if ( !OKToDJ ) {
+      //   const theUsername = await userFunctions.getUsername( theUserID )
+      //   const theMessage = `@${theUsername} is not allowed to DJ so was removed. Please speak to a Mod to find out why`;
+      //   await userFunctions.removeDJ( theUserID, theMessage, socket );
+      //   // await userFunctions.incrementSpamCounter( theUserID, databaseFunctions );
+      //   await chatFunctions.botSpeak( theMessage );
+      // } else {
 
         // sets djs current song count to zero when they enter the stage. unless they used the refresh command, in
         // which case its set to what it was before they left the room
@@ -35,7 +35,7 @@ export default async ( currentState, payload, socket, userFunctions, roomFunctio
         if ( await userFunctions.isUserInRefreshList( theUserID ) ) {
           await userFunctions.removeRefreshFromUser( theUserID, databaseFunctions );
         }
-      }
+      // }
 
       // check to see if conditions are met for bots autodjing feature
       // await botFunctions.checkAutoDJing( userFunctions );

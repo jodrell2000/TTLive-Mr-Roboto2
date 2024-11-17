7c62df41abc416b7a157e2217227c3d3c171a588
Adam Reynolds <github@smufflersworld.com>
2024-11-17 15:40:15 +0000

    Fix adding new chat commands

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -17,3 +17,97 @@ export default async ( currentState, payload, socket, userFunctions, roomFunctio
       console.warn('User profile does not have a nickname:', JSON.stringify(payload,null,2));
     }  }
 }
+
+// Ghost example
+// {
+//   "name": "userJoined",
+//     "statePatch": [
+//     {
+//       "op": "replace",
+//       "path": "/vibeMeter",
+//       "value": 0.1111111111111111
+//     },
+//     {
+//       "op": "add",
+//       "path": "/floorUsers/7",
+//       "value": {
+//         "uuid": "7327359a-b420-412d-b8a2-db492534161e",
+//         "tokenRole": "user",
+//         "canDj": true
+//       }
+//     },
+//     {
+//       "op": "add",
+//       "path": "/audienceUsers/7",
+//       "value": {
+//         "uuid": "7327359a-b420-412d-b8a2-db492534161e",
+//         "tokenRole": "user",
+//         "canDj": true
+//       }
+//     },
+//     {
+//       "op": "replace",
+//       "path": "/allUsers/8/tokenRole",
+//       "value": "user"
+//     },
+//     {
+//       "op": "replace",
+//       "path": "/allUsers/8/uuid",
+//       "value": "7327359a-b420-412d-b8a2-db492534161e"
+//     },
+//     {
+//       "op": "add",
+//       "path": "/allUsers/9",
+//       "value": {
+//         "uuid": "cab98d10-c3e2-40ea-8045-8e34009b52b9",
+//         "tokenRole": "guest",
+//         "canDj": true
+//       }
+//     },
+//     {
+//       "op": "add",
+//       "path": "/allUserData/7327359a-b420-412d-b8a2-db492534161e",
+//       "value": {
+//         "userProfile": {
+//           "id": 198747,
+//           "color": null,
+//           "avatarId": "23",
+//           "badges": [],
+//           "createdAt": "2024-11-15T04:18:03.664Z",
+//           "discord": "",
+//           "nickname": "MrWest",
+//           "discordNickname": "",
+//           "firstName": "",
+//           "lastName": "",
+//           "pronoun": "",
+//           "displayPronouns": true,
+//           "songOfTheWeek": "",
+//           "song": {
+//             "id": "",
+//             "isrc": "",
+//             "genre": "",
+//             "duration": 0,
+//             "trackUrl": "",
+//             "trackName": "",
+//             "artistName": "",
+//             "musicProvider": ""
+//           },
+//           "coverPicture": "",
+//           "facebook": "",
+//           "instagram": "",
+//           "twitter": "",
+//           "snapchat": "",
+//           "tiktok": "",
+//           "priceForGig": 0,
+//           "zodiac": "",
+//           "uuid": "7327359a-b420-412d-b8a2-db492534161e"
+//         },
+//         "position": {
+//           "x": 55,
+//           "y": 28.3
+//         },
+//         "songVotes": {}
+//       }
+//     }
+//   ]
+// }

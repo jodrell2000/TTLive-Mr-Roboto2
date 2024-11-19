0d532f3fd172f8f0bf888d1c6acbb8e94a454409
Adam Reynolds <github@smufflersworld.com>
2024-11-19 11:00:43 +0000

    Fix adding new chat commands

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -6,21 +6,20 @@ export default async ( currentState, payload, socket, userFunctions, roomFunctio
   const userInfos = await Promise.all(
     newUsers.map( async uuid => await userFunctions.extractUserInfo( payload.statePatch, uuid ) )
   );
-  console.log( `userInfos: ${ JSON.stringify( userInfos, null, 2 ) }` )
 
-  for ( const userInfo of userInfos ) {
-    let userProfile
-    if ( userInfo.avatarId === "ghost" ) {
-      userProfile = { uuid: userInfo.uuid, nickname: userInfo.nickname, avatarId: userInfo.avatarId }
-      await chatFunctions.botSpeak( "Who you gonna call?!?" )
-      //build a userInfo object for the ghost
+  for (const userInfo of userInfos) {
+    const isGhost = userInfo.avatarId === "ghost";
 
+    const userProfile = isGhost
+      ? { uuid: userInfo.uuid, nickname: userInfo.nickname, avatarId: userInfo.avatarId }
+      : await userFunctions.getUserProfileFromAPI(userInfo.uuid);
+
+    await userFunctions.userJoinsRoom(userProfile, roomFunctions, databaseFunctions, chatFunctions);
+
+    if (!isGhost) {
+      await chatFunctions.userGreeting(userInfo.uuid, userInfo.nickname, roomFunctions, userFunctions, databaseFunctions);
     } else {
-      userProfile = await userFunctions.getUserProfileFromAPI( userInfo.uuid )
-      await chatFunctions.userGreeting( userInfo.uuid, userInfo.nickname, roomFunctions, userFunctions, databaseFunctions );
-      console.log( `userProfile: ${ JSON.stringify( userProfile, null, 2 ) }` )
+      await chatFunctions.botSpeak("Who you gonna call?!?");
     }
-
-    await userFunctions.userJoinsRoom( userProfile, roomFunctions, databaseFunctions, chatFunctions );
   }
 }

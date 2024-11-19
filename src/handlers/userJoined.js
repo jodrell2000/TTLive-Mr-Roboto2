737c667a195397236cf37cc0f908fbe9ee2b6239
Adam Reynolds <github@smufflersworld.com>
2024-11-19 10:33:36 +0000

    Fix adding new chat commands

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -2,40 +2,31 @@ import { logger } from "../utils/logging.js";
   
 export default async ( currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions ) => {
   logger.debug( `=========================== userJoined.js ===========================` )
-  console.log( `payload: ${JSON.stringify(payload, null, 2)}` )
+  console.log( `payload: ${ JSON.stringify( payload, null, 2 ) }` )
 
   const newUsers = await userFunctions.findNewUserUUID( currentState )
-  console.log(`newUsers: ${JSON.stringify(newUsers, null, 2)}`)
+  console.log( `newUsers: ${ JSON.stringify( newUsers, null, 2 ) }` )
 
   const userInfos = await Promise.all(
-    newUsers.map(async uuid => await userFunctions.extractUserInfo(payload.statePatch, uuid))
+    newUsers.map( async uuid => await userFunctions.extractUserInfo( payload.statePatch, uuid ) )
   );
-  console.log(`userInfos: ${JSON.stringify(userInfos, null, 2)}`)
-  
-  for (const uuid of newUsers) {
-    const userProfile = await userFunctions.getUserProfileFromAPI( uuid )
-    // const nickname = userProfile?.nickname;
+  console.log( `userInfos: ${ JSON.stringify( userInfos, null, 2 ) }` )
 
-    let nickname = payload.statePatch
-      .filter(
-        patch =>
-          patch.op === "add" &&
-          patch.path === `/allUserData/${uuid}`
-      )
-      .map(patch => patch.value.userProfile.nickname)[0];
+  for ( const userInfo of userInfos ) {
+    let userProfile
+    if ( userInfo.avatarId === "ghost" ) {
+      //build a userInfo object for the ghost
 
-    console.log(`================== new user joined: ${nickname}`)
-
-    if (nickname) {
-      try {
-        await userFunctions.userJoinsRoom(userProfile, roomFunctions, databaseFunctions, chatFunctions);
-        await chatFunctions.userGreeting(uuid, nickname, roomFunctions, userFunctions, databaseFunctions);
-      } catch (error) {
-        console.error('Error handling user join:', error);
-      }
     } else {
-      // console.warn(`This may be a Ghost...payload: ${JSON.stringify(payload,null,2)}
-      // currentState: ${JSON.stringify(currentState,null,2)}
-      // newUsers: ${JSON.stringify(await userFunctions.findNewUserUUID( currentState ),null,2)}`);
-    }  }
+      userProfile = await userFunctions.getUserProfileFromAPI( userInfo.uuid )
+      console.log( `userProfile: ${ JSON.stringify( userProfile, null, 2 ) }` )
+    }
+
+    try {
+      await userFunctions.userJoinsRoom( userProfile, roomFunctions, databaseFunctions, chatFunctions );
+      await chatFunctions.userGreeting( userInfo.uuid, userInfo.nickname, roomFunctions, userFunctions, databaseFunctions );
+    } catch ( error ) {
+      console.error( 'Error handling user join:', error );
+    }
+  }
 }

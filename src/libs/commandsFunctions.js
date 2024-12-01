da0f7a0bcf953f6589c9d031fe1e5b577a18db17
Adam Reynolds <github@smufflersworld.com>
2024-12-01 12:22:26 +0000

    Hooking up AI, again?!?

--- src/libs/commandsFunctions.js
+++ src/libs/commandsFunctions.js
@@ -471,7 +471,7 @@ const commandFunctions = () => {
   }
   moderatorCommands.single.help = "Sets DJs to play 1 tracks each";
   
-  moderatorCommands.question = ( { args, chatFunctions, mlFunctions } ) => {
+  moderatorCommands.question = ( { data, args, chatFunctions, mlFunctions } ) => {
       mlFunctions.askGoogleAI( reassembleArgs( args, chatFunctions ) );
   }
   moderatorCommands.question.help = "Talk to Robo";

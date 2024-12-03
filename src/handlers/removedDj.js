f8943c1091102ea282907fd9a4f263135d860cff
Adam Reynolds <github@smufflersworld.com>
2024-12-03 11:27:00 +0000

    Hooking up AI, again?!?

--- src/handlers/removedDj.js
+++ src/handlers/removedDj.js
@@ -1,18 +1,8 @@
 export default async ( currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions ) => {
   let theUserID;
   for (const patch of payload.statePatch) {
-    if (["replace"].includes(patch.op)) {
-      console.log(`Replace found!!!! payload: ${JSON.stringify(payload, null, 2)}`);
-    }
-    if (["replace", "remove", "add"].includes(patch.op) && patch.path.startsWith('/audienceUsers/')) {
-      // Check if patch.value is an object and contains uuid
-      theUserID = typeof patch.value === 'object' && patch.value !== null ? patch.value.uuid : patch.value;
-
-      if (theUserID === undefined) {
-        console.log(`undefined theUserID: patch ${JSON.stringify(patch, null, 2)}`);
-      } else {
-        console.log(`UserID: ${JSON.stringify(theUserID, null, 2)}`);
-      }
+    if (["remove"].includes(patch.op)) {
+      console.log(`Remove found!!!! payload: ${JSON.stringify(payload, null, 2)}`);
     }
   }
   

aa711742aa178771b3aa7f725116cf956427d882
Adam Reynolds <github@smufflersworld.com>
2025-01-03 10:35:41 +0000

    Fix songInfoCommand

--- src/libs/songFunctions.js
+++ src/libs/songFunctions.js
@@ -311,6 +311,7 @@ const songFunctions = () => {
     songID: async () => await songID,
 
     songInfoCommand: async function ( data, databaseFunctions, chatFunctions ) {
+      console.log('this:', this); // Logs the value of `this`
       console.log(`this.songID: ${JSON.stringify( await this.songID(), null, 2)}`);
       // if ( await databaseFunctions.checkVideoDataExists( this.ytid() ) ) {
       //   await databaseFunctions.getSongInfoData( this.ytid() )

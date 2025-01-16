751088bdd1df7396d2206ad7bd87297eedbffc9b
Adam Reynolds <github@smufflersworld.com>
2025-01-16 16:05:08 +0000

    Starting to build playlist functions so that Robo can DJ again

--- src/libs/playlistFunctions.js
+++ src/libs/playlistFunctions.js
@@ -4,20 +4,19 @@ const playlistFunctions = ( ) => {
   return {
 
     getPlaylists: async function ( data, chatFunctions ) {
-      const url = "https://playlists.prod.tt.fm/crate/user"
+      const url = `https://playlists.prod.tt.fm/crate/user?limit=0&offset=0`;
       const headers = {
         'accept': 'application/json',
-        'Authorization': `Bearer ${ process.env.TTL_USER_TOKEN }`
+        'Authorization': `Bearer ${process.env.TTL_USER_TOKEN}`
       };
 
       try {
-        console.log( `crates` + JSON.stringify( await axios.get(url, { headers }) ) );
-
-      } catch ( error ) {
+        const { data } = await axios.get(url, { headers });
+        console.log(JSON.stringify(data, null, 2));
+      } catch (error) {
         console.error( `Error calling post api...error:\n${JSON.stringify(error,null,2)}\nurl:${url}\npayload:${JSON.stringify(payload,null,2)}` );
         throw error;
       }
-
     },
 
     createPlaylist: async function ( data, playlistName, chatFunctions ) {

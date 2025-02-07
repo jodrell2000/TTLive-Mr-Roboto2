927cf493b55b94da3a0447429c283539e7ef46d4
Adam Reynolds <github@smufflersworld.com>
2025-02-07 12:14:14 +0000

    Can Robo DJ?

--- src/libs/startup.js
+++ src/libs/startup.js
@@ -2,7 +2,7 @@ import { logger } from "../utils/logging.js";
 
 export default async ( roomUUID, state, roomFunctions, userFunctions, chatFunctions, songFunctions, botFunctions, databaseFunctions ) => {
   // logger.debug(`+++++++++++++++++++++++++ startup.js +++++++++++++++++++++++++` )
-  // console.log( `startup state: ${ JSON.stringify( state, null, 2) }` )
+  console.log( `startup state: ${ JSON.stringify( state, null, 2) }` )
   botFunctions.setBotStartTime()
   try {
     await roomFunctions.storeRoomData( roomUUID )

40e72c8b05262d2b0e1b389b4e2560e6ac514b39
Adam Reynolds <github@smufflersworld.com>
2025-02-28 10:54:03 +0000

    Can Robo DJ?

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -1,14 +1,21 @@
 import { logger } from "../utils/logging.js";
-  
-export default async ( currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions, mlFunctions ) => {
-  // logger.debug( `=========================== userJoined.js ===========================` )
-  const newUsers = await userFunctions.findNewUserUUID( currentState )
+
+export default async (currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions, mlFunctions) => {
+  const newUsers = await userFunctions.findNewUserUUID(currentState);
 
   const userInfos = await Promise.all(
-    newUsers.map( async uuid => await userFunctions.extractUserInfo( payload.statePatch, uuid ) )
+    newUsers.map(async uuid => {
+      const userInfo = await userFunctions.extractUserInfo(payload.statePatch, uuid);
+      if (!userInfo) {
+        console.error(`userInfo is null for UUID: ${uuid}`);
+        return null;
+      }
+      return userInfo;
+    })
   );
 
-  for (const userInfo of userInfos) {
+  for (const userInfo of userInfos.filter(user => user !== null)) {
+
     const isGhost = userInfo.avatarId === "ghost";
 
     const userProfile = isGhost

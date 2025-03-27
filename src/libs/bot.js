b5889401c4bd3145c5796b9df49b1c0a0a345777
Adam Reynolds <github@smufflersworld.com>
2025-03-27 14:02:35 +0000

    Adding in the new chat system

--- src/libs/bot.js
+++ src/libs/bot.js
@@ -8,9 +8,9 @@ import startup from '../libs/startup.js'
 import playlistFunctions from "./playlistFunctions.js";
 // import CometChatSDK from "@cometchat/chat-sdk-javascript";
 // const { CometChat } = CometChatSDK;
-global.window = {}; // Mock the window object
-import CometChatSDK from "@cometchat-pro/chat";
-const { CometChat } = CometChatSDK;
+// global.window = {}; // Mock the window object
+// import CometChatSDK from "@cometchat-pro/chat";
+// const { CometChat } = CometChatSDK;
 
 export class Bot {
   constructor( slug ) {

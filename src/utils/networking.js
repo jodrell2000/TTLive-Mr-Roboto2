301a77e856a5a60aff44613470fa86a6f7ce8abf
Adam Reynolds <github@smufflersworld.com>
2025-03-27 17:16:23 +0000

    Adding in the new chat system

--- src/utils/networking.js
+++ src/utils/networking.js
@@ -19,6 +19,7 @@ export const makeRequest = async ( url, options, extraHeaders ) => {
     ...options
   }
   
+  console.log(`url.href: ${url.href}`)
   console.log(`requestOptions: ${JSON.stringify(requestOptions, null, 2)}`)
   try {
     const response = await fetch( url.href, requestOptions )

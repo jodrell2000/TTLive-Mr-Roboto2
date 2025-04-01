f46a45d4cc58d0d6e7ad4355e24cbfb5b446c51e
Adam Reynolds <github@smufflersworld.com>
2025-04-01 10:46:35 +0100

    Add afk list to permanent memory storage

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -7,6 +7,18 @@ export default async ( payload, commandFunctions, userFunctions, videoFunctions,
   if ( await commandFunctions.wasThisACommand( payload.message ) ) {
     await commandFunctions.parseCommands( payload, userFunctions, botFunctions, roomFunctions, songFunctions, chatFunctions, videoFunctions, documentationFunctions, databaseFunctions, dateFunctions, mlFunctions, playlistFunctions, socket );
   }
-  
+
   console.log(`Someone said something: ${payload.message}`)
+  console.log(`Payload: ${JSON.stringify(payload, null, 2)}`);
+
+  // //checks to see if someone is trying to speak to an afk person or not.
+  // const foundUsernames = userFunctions.checkTextForUsernames( text );
+  //
+  // for ( let userLoop = 0; userLoop < foundUsernames.length; userLoop++ ) {
+  //   let thisAFKUserID = await userFunctions.getUserIDFromUsername( foundUsernames[ userLoop ] );
+  //   if ( await userFunctions.isUserAFK( thisAFKUserID ) && !userFunctions.isThisTheBot(  ) === true ) {
+  //     await userFunctions.sendUserIsAFKMessage( data, thisAFKUserID, chatFunctions );
+  //   }
+  // }
+
 }

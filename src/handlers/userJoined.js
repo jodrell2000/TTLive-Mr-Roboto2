a568047dd128096883bcce2fc18885c5116963d8
Adam Reynolds <github@smufflersworld.com>
2026-03-16 12:19:55 +0000

    Disable user greetings

--- src/handlers/userJoined.js
+++ src/handlers/userJoined.js
@@ -1,31 +1,31 @@
 import { logger } from "../utils/logging.js";
 
-export default async (currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions, mlFunctions) => {
-  const newUsers = await userFunctions.findNewUserUUID(currentState);
+export default async ( currentState, payload, socket, userFunctions, roomFunctions, songFunctions, chatFunctions, botFunctions, videoFunctions, databaseFunctions, documentationFunctions, dateFunctions, mlFunctions ) => {
+  const newUsers = await userFunctions.findNewUserUUID( currentState );
 
   const userInfos = await Promise.all(
-    newUsers.map(async uuid => {
-      const userInfo = await userFunctions.extractUserInfo(payload.statePatch, uuid);
-      if (!userInfo) {
-        console.error(`userInfo is null for UUID: ${uuid}`);
+    newUsers.map( async uuid => {
+      const userInfo = await userFunctions.extractUserInfo( payload.statePatch, uuid );
+      if ( !userInfo ) {
+        console.error( `userInfo is null for UUID: ${ uuid }` );
         return null;
       }
       return userInfo;
-    })
+    } )
   );
 
-  for (const userInfo of userInfos.filter(user => user !== null)) {
+  for ( const userInfo of userInfos.filter( user => user !== null ) ) {
 
     const isGhost = userInfo.avatarId === "ghost";
 
     const userProfile = isGhost
       ? { uuid: userInfo.uuid, nickname: userInfo.nickname, avatarId: userInfo.avatarId }
-      : await userFunctions.getUserProfileFromAPI(userInfo.uuid);
+      : await userFunctions.getUserProfileFromAPI( userInfo.uuid );
 
-    await userFunctions.userJoinsRoom(userProfile, roomFunctions, databaseFunctions, chatFunctions);
+    await userFunctions.userJoinsRoom( userProfile, roomFunctions, databaseFunctions, chatFunctions );
 
-    if (!isGhost) {
-      await chatFunctions.userGreeting(userInfo.uuid, userInfo.nickname, roomFunctions, userFunctions, databaseFunctions, userProfile);
+    if ( !isGhost ) {
+      // await chatFunctions.userGreeting(userInfo.uuid, userInfo.nickname, roomFunctions, userFunctions, databaseFunctions, userProfile);
     }
   }
 }

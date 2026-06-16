2966481dd7c2a9574421016a16c34b17192c0f62
Adam Reynolds <github@smufflersworld.com>
2026-06-16 17:22:15 +0100

    add logger

--- src/libs/cometchat.js
+++ src/libs/cometchat.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 import { buildUrl, makeRequest } from '../utils/networking.js'
+import { logger } from "../utils/logging.js";
 // import { CometChat } from "@cometchat/chat-sdk-javascript";
 
 const startTimeStamp = Math.floor( Date.now() / 1000 )
